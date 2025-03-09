11fb3667d77a2656bb75e7adc221f02ab255967e
dat-NHD-04 <dat.nh.64cntt@ntu.edu.vn>
2025-03-09 19:28:17 +0700

    nop bai tap

---
+++ SB_HelloSpringBoot/src/main/java/nhd/edu/SB_HelloSpringBoot/ExSpringBootAApplication.java
@@ -0,0 +1,13 @@
+package nhd.edu.SB_HelloSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExSpringBootAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExSpringBootAApplication.class, args);
+	}
+
+}
---
+++ SB_HelloSpringBoot/src/test/java/nhd/edu/ExSpringBootA/ExSpringBootAApplicationTests.java
@@ -0,0 +1,13 @@
+package nhd.edu.ExSpringBootA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExSpringBootAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
