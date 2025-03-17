16b69399d0b0acb915737f1c3000a6eabd34eccb
dat-NHD-04 <dat.nh.64cntt@ntu.edu.vn>
2025-03-17 17:56:46 +0700

    SB_RequestSpringBoot

---
+++ SB_RequestSpringBoot/src/main/java/edu/nhd/SB_RequestSpringBoot/SbRequestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package edu.nhd.SB_RequestSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRequestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRequestSpringBootApplication.class, args);
+	}
+
+}
---
+++ SB_RequestSpringBoot/src/test/java/edu/nhd/SB_RequestSpringBoot/SbRequestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nhd.SB_RequestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRequestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
