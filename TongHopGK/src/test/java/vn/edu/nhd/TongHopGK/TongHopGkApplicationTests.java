2b931b3c6b13e32b80300693a5f81a467cc3bef8
dat-NHD-04 <dat.nh.64cntt@ntu.edu.vn>
2025-03-25 09:15:37 +0700

    TongHopGK

---
+++ TongHopGK/src/test/java/vn/edu/nhd/TongHopGK/TongHopGkApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.nhd.TongHopGK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongHopGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
