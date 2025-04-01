3c5eb2cca7c5fffb75b62fd33dc5e8843d00e386
dat-NHD-04 <dat.nh.64cntt@ntu.edu.vn>
2025-04-01 09:05:28 +0700

    push lan 1

---
+++ nguyenhuydat_ThiGK/src/main/java/thiGK/ntu64130282/nguyenhuydat_ThiGK/NguyenhuydatThiGkApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130282.nguyenhuydat_ThiGK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenhuydatThiGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenhuydatThiGkApplication.class, args);
+	}
+
+}
---
+++ nguyenhuydat_ThiGK/src/test/java/thiGK/ntu64130282/nguyenhuydat_ThiGK/NguyenhuydatThiGkApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130282.nguyenhuydat_ThiGK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenhuydatThiGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
